23ef52065204a86a35adf569f6394dccb0e53762
Sébastien Guimmara <sguimmara@pobox.com>
2023-05-31 21:08:31 +0200

    index

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import Context from './src/Context';
 import Mesh from './src/objects/Mesh';
 import chroma from 'chroma-js';
 import GeometryBuilder from './src/geometries/GeometryBuilder';
-import Vector2 from './src/Vec2';
+import Vec2 from './src/Vec2';
 import Texture from './src/textures/Texture';
 import BasicMaterial from './src/materials/BasicMaterial';
 import InvertColors from './src/materials/postprocessing/InvertColors';
@@ -12,7 +12,6 @@ import SinWave from './src/materials/postprocessing/SinWave';
 
 let canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-let texture: Texture;
 const url = 'titmouche.jpg';
 
 function load8bitImage(img: HTMLImageElement, url: string): Promise<Texture> {
@@ -43,32 +42,33 @@ function load8bitImage(img: HTMLImageElement, url: string): Promise<Texture> {
 async function main() {
     const img = new Image();
 
-    texture = await load8bitImage(img, url);
+    const mouche = await load8bitImage(img, 'titmouche.jpg');
+    const logo = await load8bitImage(img, 'webgpu.png');
 
     const context = await Context.create(canvas);
     const renderer = context.renderer;
     renderer.clearColor = chroma('pink');
     renderer.setRenderStages([
-        new SinWave(),
+        new SinWave({ speed: 2 }),
         new Colorimetry({ saturation: 0.5 }),
     ]);
 
     const materials = [
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('blue')),
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('red')),
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('pink')),
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('green')),
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('cyan')),
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('white')),
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('black')),
-        new BasicMaterial().withColorTexture(texture).withDiffuseColor(chroma('purple')),
+        new BasicMaterial().withColorTexture(mouche).withDiffuseColor(chroma('blue')),
+        new BasicMaterial().withColorTexture(logo).withDiffuseColor(chroma('red')),
+        new BasicMaterial().withColorTexture(mouche).withDiffuseColor(chroma('pink')),
+        new BasicMaterial().withColorTexture(logo).withDiffuseColor(chroma('green')),
+        new BasicMaterial().withColorTexture(mouche).withDiffuseColor(chroma('cyan')),
+        new BasicMaterial().withColorTexture(logo).withDiffuseColor(chroma('white')),
+        new BasicMaterial().withColorTexture(mouche).withDiffuseColor(chroma('black')),
+        new BasicMaterial().withColorTexture(logo).withDiffuseColor(chroma('purple')),
     ];
 
     const meshes : Mesh[] = [];
 
     for (let i = 0; i < 20; i++) {
-        const center = new Vector2((Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2);
-        const size = new Vector2(Math.random() * 0.3, Math.random() * 0.3);
+        const center = new Vec2((Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2);
+        const size = new Vec2(Math.random() * 0.3, Math.random() * 0.3);
         const material = materials[Math.round(Math.random() * (materials.length - 1))];
         const mesh = new Mesh({
             material,
